d565ce19a38c1e44c49c6f6c9ee4d1f9fe58fece
gruand <gruand@gmail.com>
2024-02-28 19:36:34 +0200

    Implement send email(not working app), some changes

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import requests
 
+from send_email import send_email
+
 API_KEY = '667c5435450b4aec80d0283f0765a3b1'
 query = "Ukraine"
 url = "https://newsapi.org/v2/everything?q=" + query + "&from=2024-01-27&sortBy=publishedAt&apiKey=" + API_KEY
 
-request = requests.get(url)
-content = request.json()
+response = requests.get(url)
+content = response.json()
 
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article is not None:
+        body = body + article["title"] + "\n" \
+               + article["description"] + "\n" \
+               + article["url"] + 2 * "\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
