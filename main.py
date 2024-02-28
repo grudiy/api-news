56ac8fc9b00e42a83c134878fbac9c54d7545adb
gruand <gruand@gmail.com>
2024-02-28 19:37:17 +0200

    Implement send email(not working app), some changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 from send_email import send_email
 
-API_KEY = '667c5435450b4aec80d0283f0765a3b1'
+API_KEY = '667c5435450b4aec80d0283XXX'
 query = "Ukraine"
 url = "https://newsapi.org/v2/everything?q=" + query + "&from=2024-01-27&sortBy=publishedAt&apiKey=" + API_KEY
 
